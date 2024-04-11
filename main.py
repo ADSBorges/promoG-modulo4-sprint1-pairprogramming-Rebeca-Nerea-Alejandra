f5850ee13ca15a098d0bacb5783019f811dee098
rebeca-enma <rebeca.enma@gmail.com>
2024-04-11 10:35:36 +0200

    Actualizacion CSV

---
+++ main.py
@@ -0,0 +1,14 @@
+#%%
+from src import soporte as sp
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# %%
+
+df_brecha = pd.read_excel("files/brecha_salarial_UE.xlsx")
+df_brecha
+
+
+
+# %%
